c1c47b76587746ae22f512d28a2d63c242bff363
smndtrl <smndtrl@noreply.users.github.com>
2023-05-05 12:08:07 +0000

    initial SPNEGO crate

---
+++ spnego/src/lib.rs
@@ -0,0 +1,17 @@
+#![no_std]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![doc = include_str!("../README.md")]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/6ee8e381/logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/6ee8e381/logo.svg"
+)]
+#![forbid(unsafe_code)]
+#![warn(
+    clippy::mod_module_files,
+    clippy::unwrap_used,
+    missing_docs,
+    rust_2018_idioms,
+    unused_qualifications
+)]
+
+//! TODO: SPNEGO crate
